332ff34f4f94c728094e93e798c1edf83cd2eb44
Assalyk <karim.assaly@epitech.eu>
2020-02-03 14:20:03 +0100

    removed every html page and things related to html display in main.go and home.go(handler) so it doesn't show 404 not found on http terminal testing

--- handlers/home.go
+++ handlers/home.go
@@ -1,12 +1,1 @@
 package handlers
-
-import (
-	"net/http"
-	"path"
-)
-
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	p := path.Dir("./static/*")
-	w.Header().Set("Content-type", "text/html")
-	http.ServeFile(w, r, p)
-}
--- main.go
+++ main.go
@@ -12,7 +12,6 @@ func main() {
 	router := mux.NewRouter()
 	db, _ := database.InitDB()
 	database.CreateTable(db)
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
 	router.HandleFunc("/register", handlers.RegisterHandler).Methods("POST")
 	router.HandleFunc("/login", handlers.LoginHandler).Methods("POST")
 	http.ListenAndServe(":8080", router)
